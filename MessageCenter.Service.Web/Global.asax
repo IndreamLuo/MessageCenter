faf51810ab9506a336e0968f79f7a2f8d1d82ad3
Indream Luo <indreamluo@qq.com>
2016-01-19 15:30:59 +0800

    First Build

---
+++ MessageCenter.Service.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MessageCenter.Service.Web.MvcApplication" Language="C#" %>
---
+++ MessageCenter.Service.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessageCenter.Service.Web.Startup))]
+namespace MessageCenter.Service.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
